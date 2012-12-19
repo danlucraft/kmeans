e19452f4b3f1918cd00b8b4f113ad2691bf96328
Daniel Lucraft <dan@fluentradical.com>
2012-12-19 09:05:47 +0000

    Validate and parse input file

--- lib/kmeans/cli.rb
+++ lib/kmeans/cli.rb
@@ -3,20 +3,36 @@ module Kmeans
   class CLI
     def initialize(args)
       @args = args
-      unless input_path
-        puts "USAGE bin/kmeans FILE"
-        exit 1
-      end
     end
 
     def input_path
       @args.detect {|arg| File.exist?(arg) && File.file?(arg) }
     end
 
+    FLOAT_MATCHER = /(-?[0-9]+(?:\.[0-9]+)?)/
+
     def input_vectors
+      @input_vectors ||= begin
+        lines = File.readlines(input_path)
+        vectors = []
+        lines.each do |line|
+          line = line.chomp
+          if line =~ /^#{FLOAT_MATCHER},#{FLOAT_MATCHER}$/
+            vectors << [$1.to_f, $2.to_f]
+          else 
+            $stderr.puts "unknown line format: '#{line}'"
+          end
+        end
+        vectors
+      end
     end
 
     def run
+      unless input_path
+        puts "USAGE: bin/kmeans FILE"
+        exit 1
+      end
+      puts "Got #{input_vectors.length} vectors"
     end
   end
 end
