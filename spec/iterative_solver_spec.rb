143474b24f744b22a936a8810d497d11d7516681
Daniel Lucraft <dan@fluentradical.com>
2012-12-19 09:13:24 +0000

    Add spec stubs

---
+++ spec/iterative_solver_spec.rb
@@ -0,0 +1,8 @@
+
+require 'spec_helper'
+
+describe Kmeans::IterativeSolver do
+  it "Should exist" do
+    Kmeans::IterativeSolver.should_not be_nil
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+
+$:.push(File.expand_path("../../lib", __FILE__))
+require 'kmeans'
+
+
