4f56342b4d5ef672cbd3ca49f3c5a48d41bdcee2
Daniel Lucraft <dan@fluentradical.com>
2012-12-19 09:22:06 +0000

    Picks random means

--- lib/kmeans/iterative_solver.rb
+++ lib/kmeans/iterative_solver.rb
@@ -1,8 +1,15 @@
 
 module Kmeans
   class IterativeSolver
-    def initialize(data)
-      @data = data
+    attr_reader :means
+
+    def initialize(k, data)
+      @k, @data = k, data
+      @means = random_values(k)
+    end
+
+    def random_values(n)
+      @data.shuffle[0...n]
     end
 
     def cluster_means
--- spec/iterative_solver_spec.rb
+++ spec/iterative_solver_spec.rb
@@ -2,7 +2,24 @@
 require 'spec_helper'
 
 describe Kmeans::IterativeSolver do
-  it "Should exist" do
-    Kmeans::IterativeSolver.should_not be_nil
+
+  describe "initial mean picking" do
+    before do
+      @values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      solver = Kmeans::IterativeSolver.new(3, @values)
+      @means = solver.means
+    end
+
+    it "should pick the right number of means" do
+      @means.length.should == 3
+    end
+
+    it "should pick from the set given" do
+      (@means - @values).should be_empty
+    end
+
+    it "should choose distinct values" do
+      @means.sort.uniq.length.should == @means.length
+    end
   end
 end
