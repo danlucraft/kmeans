30a64facbccf495e26caaade2f5f9d5a969f595b
Daniel Lucraft <dan@fluentradical.com>
2012-12-19 10:05:57 +0000

    Working solver, I think

--- lib/kmeans/iterative_solver.rb
+++ lib/kmeans/iterative_solver.rb
@@ -1,15 +1,60 @@
 
 module Kmeans
   class IterativeSolver
-    attr_reader :means
+    attr_reader :clusters, :data
+
+    class Cluster
+      attr_reader :mean, :values
+
+      def initialize(mean)
+        @mean = mean
+        @values = []
+      end
+
+      def update_mean
+        @mean = observed_mean
+        @values = []
+        @observed_mean = nil
+      end
+
+      def observed_mean
+        @observed_mean ||= @values.inject {|m,o| m + o }/@values.length.to_f
+      end
+
+      def add(value)
+        @values << value
+      end
+    end
 
     def initialize(k, data)
       @k, @data = k, data
-      @means = random_values(k)
+      @clusters = random_values(k).map {|m| Cluster.new(m) }
+      @data.each do |value|
+        cluster = clusters.sort_by { |cluster| (value - cluster.mean).magnitude }.first
+        cluster.add(value)
+      end
+    end
+
+    def means
+      @clusters.map(&:mean)
     end
 
     def random_values(n)
       @data.shuffle[0...n]
+    end
+
+    def signature
+      clusters.map {|c| [c.observed_mean, c.values.length] }
+    end
+
+    def iterate
+      prev_sig = signature
+      clusters.each(&:update_mean)
+      @data.each do |value|
+        cluster = clusters.sort_by { |cluster| (value - cluster.mean).magnitude }.first
+        cluster.add(value)
+      end
+      prev_sig == signature
     end
   end
 end
--- spec/iterative_solver_spec.rb
+++ spec/iterative_solver_spec.rb
@@ -3,23 +3,34 @@ require 'spec_helper'
 
 describe Kmeans::IterativeSolver do
 
-  describe "initial mean picking" do
-    before do
-      @values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      solver = Kmeans::IterativeSolver.new(3, @values)
-      @means = solver.means
-    end
+  before do
+    @values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @solver = Kmeans::IterativeSolver.new(3, @values)
+    @means = @solver.means
+  end
+
+  describe "initial cluster picking" do
 
     it "should pick the right number of means" do
       @means.length.should == 3
     end
 
-    it "should pick from the set given" do
+    it "should pick means from the set given" do
       (@means - @values).should be_empty
     end
 
-    it "should choose distinct values" do
+    it "should choose distinct means" do
       @means.sort.uniq.length.should == @means.length
+    end
+
+    it "should assign all data to clusters" do
+      @solver.clusters.map(&:values).flatten.sort.should == @values
+    end
+  end
+
+  describe "update_means" do
+    before do
+
     end
   end
 end
